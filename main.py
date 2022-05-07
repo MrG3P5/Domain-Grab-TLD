1057eff2f27e8098121c35afa04da4ad830b39ff
X - MrG3P5 <57594747+MrG3P5@users.noreply.github.com>
2022-05-08 01:42:13 +0700

    y
    y

---
+++ main.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# Created By X-MrG3P5
+
+import requests, os
+from re import findall as reg
+from string import ascii_lowercase
+
+
+class Grab_TLD:
+    def __start__(self, extension):
+        arr_alphabet = list(ascii_lowercase)
+        for y in arr_alphabet:
+            req = requests.get(f"https://www.pagesinventory.com/tld/{extension}/{y}.html")
+        
+            if req.status_code == 200:
+                regx = reg('<td><a href="\\/domain\\/(.*?).html">', req.text)
+                print(f"[*] Grabbed {len(regx)} Domain")
+
+                for x in regx:
+                    if x in open(f"resut_{extension}.txt", "r").read():
+                        pass
+                    else:
+                        open(f"resut_{extension}.txt", "a").write(x + "\n")
+
+
+if __name__=="__main__":
+    os.system("cls||clear")
+    tld_input = input("[?] Domain (ex: com) : ")
+
+    try:
+        open(f"resut_{tld_input}.txt", "a")
+    except:
+        pass
+
+    Grab_TLD().__start__(tld_input)
