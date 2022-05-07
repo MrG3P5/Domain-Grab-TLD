78ae1628f539b1ba8510131e2600ddd11bbe1c55
X - MrG3P5 <57594747+MrG3P5@users.noreply.github.com>
2022-05-08 01:55:17 +0700

    try catch added
    try catch added

--- main.py
+++ main.py
@@ -10,18 +10,21 @@ class Grab_TLD:
     def __start__(self, extension):
         arr_alphabet = list(ascii_lowercase)
         for y in arr_alphabet:
-            req = requests.get(f"https://www.pagesinventory.com/tld/{extension}/{y}.html")
+            try:
+                req = requests.get(f"https://www.pagesinventory.com/tld/{extension}/{y}.html")
         
-            if req.status_code == 200:
-                regx = reg('<td><a href="\\/domain\\/(.*?).html">', req.text)
-                print(f"[*] Grabbed {len(regx)} Domain")
+                if req.status_code == 200:
+                    regx = reg('<td><a href="\\/domain\\/(.*?).html">', req.text)
+                    print(f"[*] Grabbed {len(regx)} Domain")
 
-                for x in regx:
-                    if x in open(f"resut_{extension}.txt", "r").read():
-                        pass
-                    else:
-                        open(f"resut_{extension}.txt", "a").write(x + "\n")
+                    for x in regx:
+                        if x in open(f"resut_{extension}.txt", "r").read():
+                            pass
+                        else:
+                            open(f"resut_{extension}.txt", "a").write(x + "\n")
 
+            except:
+                print(f"[*] Domain Data Not Found In Page")
 
 if __name__=="__main__":
     os.system("cls||clear")
